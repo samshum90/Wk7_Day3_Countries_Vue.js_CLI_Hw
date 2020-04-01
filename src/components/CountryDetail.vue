e6a459bc07883d95a2a98ee16b2fc272c3ad07c0
Sam <samhum90@gmail.com>
2020-04-01 23:49:33 +0100

    first commit

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if='country'>
+    <h3>{{country.name}}</h3>
+    <p>Capital: {{country.capital}}</p>
+    <p>Population: {{country.population}}</p>
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../main.js';
+export default {
+    name:'country-detail',
+    props: ['country']
+}
+</script>
+
+<style>
+
+</style>
+
